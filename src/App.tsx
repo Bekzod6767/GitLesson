60d2cf5b72d233d7eaf8550f2f18346d1596e3df
Bekzod <vorkonmurov@gmail.com>
2026-05-23 20:34:53 +0500

    appda ozgarishla boldi

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <ul>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>Vite</li>
+      </ul>
+    </div>
+  )
+}
