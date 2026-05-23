213d735fd70e9cfa6867128ac4481264b3057a78
Bekzod <vorkonmurov@gmail.com>
2026-05-23 20:15:23 +0500

    navbarga elementlar qoshildi

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Navbar() {
+  return (
+    <div>
+        <ul>
+            <li>
+                "Website nomi"
+                "Home"
+                "Aloqa"
+                "Kontakt"
+                "Biz haqimizda"
+            </li>
+        </ul>
+    </div>
+  )
+}
